7558509c573a4cd7f6b6e5c4eadbc365b33b1fcb
Joon Hwan 김준환 <xncbf12@gmail.com>
2021-04-17 19:03:32 +0900

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -96,8 +96,8 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-robots-txt',
       options: {
-        host: 'https://your-blog.netlify.app',
-        sitemap: 'https://your-blog.netlify.app/sitemap.xml',
+        host: 'https://pypy.dev',
+        sitemap: 'https://pypy.dev/sitemap.xml',
         policy: [
           {
             userAgent: '*',
